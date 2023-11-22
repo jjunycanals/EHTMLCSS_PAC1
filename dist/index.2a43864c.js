194055c502a85d24b64135b05a49405afba72a30
Jordi Juny <jjunyc@uoc.edu>
2023-11-22 19:16:25 +0100

    run build

--- dist/index.2a43864c.js
+++
@@ -1,8 +0,0 @@
-var t,a,l;t=document.getElementById("nav"),a=(l=window.location.pathname.split("/"))[l.length-1],t.innerHTML=`
-<li ${"main.html"===a?'class="active"':""}><a href="./main.html">Main</a></li>
-<li ${"category.html"===a?'class="active"':""}><a href="./category.html">Category</a></li>
-<li ${"content.html"===a?'class="active"':""}><a href="./content.html">Content</a></li>
-<li ${"links.html"===a?'class="active"':""}><a href="./links.html">Links</a></li>
-`;//# sourceMappingURL=index.2a43864c.js.map
-
-//# sourceMappingURL=index.2a43864c.js.map
--- src/nav.js
+++ src/nav.js
@@ -2,10 +2,10 @@ var nav = document.getElementById('nav');
 var currentPage = getCurrentPageName();
 
 nav.innerHTML = `
-<li ${currentPage === 'main.html' ? 'class="active"' : ''}><a href="./main.html">Main</a></li>
-<li ${currentPage === 'category.html' ? 'class="active"' : ''}><a href="./category.html">Category</a></li>
-<li ${currentPage === 'content.html' ? 'class="active"' : ''}><a href="./content.html">Content</a></li>
-<li ${currentPage === 'links.html' ? 'class="active"' : ''}><a href="./links.html">Links</a></li>
+<li ${currentPage === 'main.html' ? 'class="active"' : ''}><a href="main.html">Main</a></li>
+<li ${currentPage === 'category.html' ? 'class="active"' : ''}><a href="category.html">Category</a></li>
+<li ${currentPage === 'content.html' ? 'class="active"' : ''}><a href="content.html">Content</a></li>
+<li ${currentPage === 'links.html' ? 'class="active"' : ''}><a href="links.html">Links</a></li>
 `;
 
 // Per obtenir la URL de la pagian que ens trobem i poder treballar amb l'element .active al NAV
