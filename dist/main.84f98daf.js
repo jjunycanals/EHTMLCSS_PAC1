e861579f2f66c79961522eb392e4b4e739b19eac
Jordi Juny <jjunyc@uoc.edu>
2023-11-23 11:21:50 +0100

    publicar arxius .js dins carpeta js

---
+++ dist/main.84f98daf.js
@@ -0,0 +1,8 @@
+var t,a=document.getElementById("nav"),l=(t=window.location.pathname.split("/"))[t.length-1];a.innerHTML=`
+<li ${"main.html"===l?'class="active"':""}><a href="main.html">Main</a></li>
+<li ${"category.html"===l?'class="active"':""}><a href="category.html">Category</a></li>
+<li ${"content.html"===l?'class="active"':""}><a href="content.html">Content</a></li>
+<li ${"links.html"===l?'class="active"':""}><a href="links.html">Links</a></li>
+`;//# sourceMappingURL=main.84f98daf.js.map
+
+//# sourceMappingURL=main.84f98daf.js.map
--- dist/main.9ab87970.js
+++
@@ -1,8 +0,0 @@
-var t,a,l;t=document.getElementById("nav"),a=(l=window.location.pathname.split("/"))[l.length-1],t.innerHTML=`
-<li ${"main.html"===a?'class="active"':""}><a href="main.html">Main</a></li>
-<li ${"category.html"===a?'class="active"':""}><a href="category.html">Category</a></li>
-<li ${"content.html"===a?'class="active"':""}><a href="content.html">Content</a></li>
-<li ${"links.html"===a?'class="active"':""}><a href="links.html">Links</a></li>
-`;//# sourceMappingURL=main.9ab87970.js.map
-
-//# sourceMappingURL=main.9ab87970.js.map
